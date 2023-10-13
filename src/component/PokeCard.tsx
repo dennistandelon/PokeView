ea7168dd9eca0f9a94795c0ec9806e232dfbbab2
Mioshido30 <reine.itsuka30@gmail.com>
2023-10-13 22:18:44 +0700

    cleaning unused component

--- src/component/PokeCard.tsx
+++ src/component/PokeCard.tsx
@@ -8,18 +8,11 @@ export default function PokeCard(props:Pokemon){
 
     const img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+id+".png";
 
-    // const handleRedirect = (name:string) =>{
-        
-    // }
-
     return (
     <div className="card w-64 md:w-96 shadow-xl mt-3 bg-base-100">
         <figure><img src={img} width={400} height={400} alt={props.name} /></figure>
         <div className="card-body bg-neutral rounded-b-2xl">
             <h2 className="card-title text-neutral-content">{props.name}</h2>
-            {/* <div className="card-actions justify-end">
-                <button className="btn btn-primary">Detail</button>
-            </div> */}
         </div>
     </div>
     )
